1f9c74bb0a1690384adf4cc8e1c526a736507025
Paulo Felipe Souza <alparakleto@gmail.com>
2024-12-29 14:53:44 -0300

    set the hidden salary value as expected from the backend
    Consider the hidden salary as the one to be used in the controller
    params.
    The masked_salary is for user visualization only.

--- app/assets/javascript/inssCalculatorView.js
+++ app/assets/javascript/inssCalculatorView.js
@@ -1,3 +1,12 @@
-let inputSalary = document.querySelector("#salary");
+let inputSalary = document.querySelector("#masked_salary");
 let masker = new VMasker(inputSalary);
+let hiddenElement = document.querySelector("#salary");
+let inssCalculatorForm = document.querySelector("#inss_calculator_form");
 masker.maskMoney();
+
+inssCalculatorForm.addEventListener("submit", (event) =>{
+  let inputSalary = document.querySelector("#masked_salary");
+  let masker = new VMasker(inputSalary);
+  console.log(masker.unMask());
+  hiddenElement.value = hiddenElement.value = parseFloat(VMasker.toNumber(inputSalary.value))/100
+});
