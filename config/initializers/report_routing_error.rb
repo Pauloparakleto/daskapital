b97030cf49857a895342252963cbd099f6207812
Paulo Felipe Souza <alparakleto@gmail.com>
2025-01-05 12:47:12 -0300

    Add interceptor for sentry exception

---
+++ config/initializers/report_routing_error.rb
@@ -0,0 +1,9 @@
+# config/initializers/report_routing_error.rb
+
+ActionDispatch::DebugExceptions.register_interceptor do |req, exception|
+  if exception.is_a? ActionController::InvalidAuthenticityToken
+    p ["InvalidAuthenticityToken Exception", req, exception]
+    Sentry.capture_exception(exception)
+    Sentry.capture_message("Add https protocol to the site")
+  end
+end
