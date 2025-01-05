76cd6b91635ec89782192eafabf270c9014b3d9b
Paulo Felipe Souza <alparakleto@gmail.com>
2025-01-05 12:50:04 -0300

    lint correction

--- config/initializers/report_routing_error.rb
+++ config/initializers/report_routing_error.rb
@@ -2,7 +2,7 @@
 
 ActionDispatch::DebugExceptions.register_interceptor do |req, exception|
   if exception.is_a? ActionController::InvalidAuthenticityToken
-    p ["InvalidAuthenticityToken Exception", req, exception]
+    p [ "InvalidAuthenticityToken Exception", req, exception ]
     Sentry.capture_exception(exception)
     Sentry.capture_message("Add https protocol to the site")
   end
