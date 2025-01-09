07ea76f5f294bf57f125a944f57af99a84cc3a3f
Paulo Felipe Souza <alparakleto@gmail.com>
2025-01-09 11:40:21 -0300

    refactor TheMask.js
    clarify variable names
    simplify the cursor position adjustment logic
    use template literal for string formatting

--- app/assets/javascript/TheMask.js
+++ app/assets/javascript/TheMask.js
@@ -1,24 +1,20 @@
 class TheMask {
-  static use(event){
-    let input = event.target;
+  static use(event) {
+    const input = event.target;
     let value = input.value.replace(/\D/g, ''); // Remove non-numeric characters
-    let cursorPosition = input.selectionStart;
-    let oldValueLength = input.value.length;
+    const oldCursorPos = input.selectionStart;
+    const oldValueLength = input.value.length;
 
-    value = (value / 100).toFixed(2) + '';
-    value = value.replace(".", ",");
-    value = value.replace(/(\d)(?=(\d{3})+\,)/g, '$1.')
-
+    value = (value / 100).toFixed(2).replace('.', ',').replace(/(\d)(?=(\d{3})+,)/g, '$1.');
     input.value = value;
-    let newValueLength = input.value.length;
-
-    // Adjust cursor position to stay in the same relative place
-    cursorPosition = Math.max(cursorPosition + (newValueLength - oldValueLength), 0);
-    input.setSelectionRange(cursorPosition, cursorPosition);
+    
+    const newValueLength = input.value.length;
+    const newCursorPos = Math.max(oldCursorPos + (newValueLength - oldValueLength), 0);
+    input.setSelectionRange(newCursorPos, newCursorPos); // Adjust cursor position
   }
 
-  static unuse(maskedNumber){
-    maskedNumber = maskedNumber.replace(".", "").replace(",", ".");
-    return parseFloat(maskedNumber);
+  static unuse(maskedValue) {
+    return parseFloat(maskedValue.replace('.', '').replace(',', '.'));
   }
 }
+
