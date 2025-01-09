46824e4822ccc0cdce7c9f07d13c3f0bfa819c22
Paulo Felipe Souza <alparakleto@gmail.com>
2025-01-09 11:32:46 -0300

    refactor inssCalculatorView
    use const for things never change.
    use arrow function for simplicity.

--- app/assets/javascript/inssCalculatorView.js
+++ app/assets/javascript/inssCalculatorView.js
@@ -1,12 +1,7 @@
-let inputSalary = document.querySelector("#masked_salary");
-let hiddenElement = document.querySelector("#salary");
-let inssCalculatorForm = document.querySelector("#inss_calculator_form");
+const inputSalary = document.querySelector("#masked_salary");
+const hiddenElement = document.querySelector("#salary");
+const inssCalculatorForm = document.querySelector("#inss_calculator_form");
 
-document.getElementById('masked_salary').addEventListener('input', function(e) {
-  TheMask.use(e);
-});
+document.getElementById('masked_salary').addEventListener('input', e => TheMask.use(e));
 
-inssCalculatorForm.addEventListener("submit", (event) =>{
-  let inputSalary = document.querySelector("#masked_salary");
-  hiddenElement.value = TheMask.unuse(inputSalary.value);
-});
+inssCalculatorForm.addEventListener("submit", () => hiddenElement.value = TheMask.unuse(inputSalary.value));
