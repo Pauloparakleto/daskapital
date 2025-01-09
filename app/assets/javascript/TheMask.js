6a909f3fe5d7059326e88617f4dc1a4a216037eb
Paulo Felipe Souza <alparakleto@gmail.com>
2025-01-09 11:40:44 -0300

    remove comments

--- app/assets/javascript/TheMask.js
+++ app/assets/javascript/TheMask.js
@@ -1,16 +1,15 @@
 class TheMask {
   static use(event) {
     const input = event.target;
-    let value = input.value.replace(/\D/g, ''); // Remove non-numeric characters
+    let value = input.value.replace(/\D/g, '');
     const oldCursorPos = input.selectionStart;
     const oldValueLength = input.value.length;
 
     value = (value / 100).toFixed(2).replace('.', ',').replace(/(\d)(?=(\d{3})+,)/g, '$1.');
     input.value = value;
-    
     const newValueLength = input.value.length;
     const newCursorPos = Math.max(oldCursorPos + (newValueLength - oldValueLength), 0);
-    input.setSelectionRange(newCursorPos, newCursorPos); // Adjust cursor position
+    input.setSelectionRange(newCursorPos, newCursorPos);
   }
 
   static unuse(maskedValue) {
