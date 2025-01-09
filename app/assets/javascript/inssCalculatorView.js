d9287a0dfa17a0163687f6ea384f4aa2337c2a8d
Paulo Felipe Souza <alparakleto@gmail.com>
2025-01-08 22:21:35 -0300

    refactor TheMask to static functions

--- app/assets/javascript/inssCalculatorView.js
+++ app/assets/javascript/inssCalculatorView.js
@@ -1,14 +1,14 @@
 let inputSalary = document.querySelector("#masked_salary");
-let masker = new VMasker(inputSalary);
 let hiddenElement = document.querySelector("#salary");
 let inssCalculatorForm = document.querySelector("#inss_calculator_form");
 
 document.getElementById('masked_salary').addEventListener('input', function(e) {
-  new TheMask(e).use();
+  TheMask.use(e);
 });
 
 inssCalculatorForm.addEventListener("submit", (event) =>{
   let inputSalary = document.querySelector("#masked_salary");
   let masker = new VMasker(inputSalary);
-  hiddenElement.value = parseFloat(VMasker.toNumber(inputSalary.value))/100
+  debugger
+  hiddenElement.value = TheMask.unuse(inputSalary.value);
 });
