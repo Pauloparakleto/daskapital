34e37953aadce9399bf1e282dbecd7d9962e4add
Paulo Felipe Souza <alparakleto@gmail.com>
2025-02-03 21:17:32 -0300

    update inss expectation
    Contribution calculates 2025

--- spec/models/calculator/inss_spec.rb
+++ spec/models/calculator/inss_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe Calculator::Inss, type: :model do
   describe "#contribution" do
     it "returns correct contribution" do
-      expect(described_class.new(salary: 3000).contribution).to eq(258.81)
+      expect(described_class.new(salary: 3000).contribution).to eq(253.4)
     end
   end
 end
