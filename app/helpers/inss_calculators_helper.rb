f81c6b35735d4b68033ccf8d333914b765f7917d
Paulo Felipe Souza <alparakleto@gmail.com>
2025-02-06 22:23:23 -0300

    move link to law to inss helper

--- app/helpers/inss_calculators_helper.rb
+++ app/helpers/inss_calculators_helper.rb
@@ -1,5 +1,8 @@
 module InssCalculatorsHelper
   def legacy_inss_law_link
+    base_url =  "https://www.in.gov.br/en/web/dou/-/portaria-interministerial-mps/mf-n-2-de-11-de-janeiro-de-2024-537035232" 
+    text = text || "Portaria Interministerial n. 2 de 11 de Janeiro de 2024"
+    link_to text, base_url, target: "_blank"
   end
 
   def inss_law_link(text = nil)
