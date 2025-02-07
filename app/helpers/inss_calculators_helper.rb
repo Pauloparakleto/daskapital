5b1ceea7347ab243a5d315173357afe98e312107
Paulo Felipe Souza <alparakleto@gmail.com>
2025-02-06 22:37:49 -0300

    link trailingwhitspace

--- app/helpers/inss_calculators_helper.rb
+++ app/helpers/inss_calculators_helper.rb
@@ -1,6 +1,6 @@
 module InssCalculatorsHelper
   def legacy_inss_law_link(text = nil)
-    base_url =  "https://www.in.gov.br/en/web/dou/-/portaria-interministerial-mps/mf-n-2-de-11-de-janeiro-de-2024-537035232" 
+    base_url =  "https://www.in.gov.br/en/web/dou/-/portaria-interministerial-mps/mf-n-2-de-11-de-janeiro-de-2024-537035232"
     text = text || "Portaria Interministerial n. 2 de 11 de Janeiro de 2024"
     link_to text, base_url, target: "_blank"
   end
