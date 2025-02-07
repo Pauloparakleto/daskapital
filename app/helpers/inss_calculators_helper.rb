a0c7abca5288b74d6f198ed7822be858b77c0069
Paulo Felipe Souza <alparakleto@gmail.com>
2025-02-06 21:17:43 -0300

    add link between 2024 and 2025 pages

--- app/helpers/inss_calculators_helper.rb
+++ app/helpers/inss_calculators_helper.rb
@@ -1,2 +1,10 @@
 module InssCalculatorsHelper
+  def legacy_inss_law_link
+  end
+
+  def inss_law_link(text = nil)
+    base_url =  "https://www.in.gov.br/en/web/dou/-/portaria-interministerial-mps/mf-n-6-de-10-de-janeiro-de-2025-606526848"
+    text = text || "Portaria Interministerial n. 6 de 10 de Janeiro de 2025"
+    link_to text, base_url, target: "_blank"
+  end
 end
